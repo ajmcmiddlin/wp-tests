2c6f04bf9a7f9eefcd1d95900ca983deb07c29cf
Andrew McCluskey <andrew@ajmccluskey.com>
2018-06-20 09:20:37 +1000

    Add createPost to API

--- src/Web/WordPress/API.hs
+++ src/Web/WordPress/API.hs
@@ -4,7 +4,7 @@
 module Web.WordPress.API where
 
 import           Data.Proxy                (Proxy (Proxy))
-import           Servant.API               ((:<|>), (:>), Get, JSON, ReqBody)
+import           Servant.API               ((:<|>) ((:<|>)), (:>), Get, JSON, ReqBody, NoContent, Post)
 import           Servant.Client            (ClientM, client)
 
 import           Web.WordPress.Types.Post  (ListPostsMap, PostMap)
@@ -12,10 +12,14 @@ import           Web.WordPress.YoloContent (YoloJSON)
 
 type Posts =
   "posts" :>
-  ( ReqBody '[JSON] ListPostsMap :> Get '[JSON, YoloJSON] [PostMap]
+  (    ReqBody '[JSON] ListPostsMap :> Get '[JSON, YoloJSON] [PostMap]
+  :<|> ReqBody '[JSON] PostMap :> Post '[JSON] NoContent
   )
 
 postsAPI = Proxy :: Proxy Posts
 
 listPosts :: ListPostsMap -> ClientM [PostMap]
-listPosts = client postsAPI
+createPost :: PostMap -> ClientM NoContent
+
+(listPosts :<|> createPost) = client postsAPI
+
