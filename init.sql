47ff4c812b269f4bd01280bd9714d22a87d959ea
Andrew McCluskey <andrew@ajmccluskey.com>
2018-06-21 11:08:05 +1000

    Try activating basic auth plugin at deploy

--- init.sql
+++ init.sql
@@ -1,3 +1,5 @@
 CREATE USER 'wordpress'@'%' IDENTIFIED BY 'wordpress';
 GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
+
+UPDATE wp_options SET option_value = 'a:1:{i:0;s:32:"basic-auth-plugin/basic-auth.php";}' where option_name = 'active_plugins';
