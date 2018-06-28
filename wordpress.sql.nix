db0d0682f8f532acfb0123df0142cf901ddcd7ff
Andrew McCluskey <andrew@ajmccluskey.com>
2018-06-28 15:51:51 +1000

    Remove session options from original dump that we don't need

--- wordpress.sql.nix
+++ wordpress.sql.nix
@@ -301,7 +301,7 @@ pkgs.writeText "wordpress.sql" ''
 
   LOCK TABLES `wp_usermeta` WRITE;
   /*!40000 ALTER TABLE `wp_usermeta` DISABLE KEYS */;
-  INSERT INTO `wp_usermeta` VALUES (1,1,'nickname','andrew'),(2,1,'first_name','''),(3,1,'last_name','''),(4,1,'description','''),(5,1,'rich_editing','true'),(6,1,'syntax_highlighting','true'),(7,1,'comment_shortcuts','false'),(8,1,'admin_color','fresh'),(9,1,'use_ssl','0'),(10,1,'show_admin_bar_front','true'),(11,1,'locale','''),(12,1,'wp_capabilities','a:1:{s:13:\"administrator\";b:1;}'),(13,1,'wp_user_level','10'),(14,1,'dismissed_wp_pointers','''),(15,1,'show_welcome_panel','1'),(16,1,'session_tokens','a:1:{s:64:\"fdde76661c7d9cd89365357fab55c7caefb58c579514762e5a4a69e7ac58cc01\";a:4:{s:10:\"expiration\";i:1528861821;s:2:\"ip\";s:12:\"192.168.56.1\";s:2:\"ua\";s:68:\"Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0\";s:5:\"login\";i:1528689021;}}'),(17,1,'wp_dashboard_quick_press_last_post_id','3'),(18,1,'community-events-location','a:1:{s:2:\"ip\";s:12:\"192.168.56.0\";}');
+  INSERT INTO `wp_usermeta` VALUES (1,1,'nickname','andrew'),(2,1,'first_name','''),(3,1,'last_name','''),(4,1,'description','''),(5,1,'rich_editing','true'),(6,1,'syntax_highlighting','true'),(7,1,'comment_shortcuts','false'),(8,1,'admin_color','fresh'),(9,1,'use_ssl','0'),(10,1,'show_admin_bar_front','true'),(11,1,'locale','''),(12,1,'wp_capabilities','a:1:{s:13:\"administrator\";b:1;}'),(13,1,'wp_user_level','10'),(14,1,'dismissed_wp_pointers','''),(15,1,'show_welcome_panel','1'),(18,1,'community-events-location','a:1:{s:2:\"ip\";s:12:\"192.168.56.0\";}');
   /*!40000 ALTER TABLE `wp_usermeta` ENABLE KEYS */;
   UNLOCK TABLES;
 
