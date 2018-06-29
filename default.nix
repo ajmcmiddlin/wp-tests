4c2e09ebd2d04e591a56a745d4a112627cb61dc5
Andrew McCluskey <andrew@ajmccluskey.com>
2018-06-29 10:24:46 +1000

    Consolidate nix files
    default.nix used with nix-shell when no shell.nix found

--- default.nix
+++ default.nix
@@ -1,6 +1,43 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default", profiling ? false }:
 
 let
-  env = import ./env.nix {inherit nixpkgs compiler; };
+  inherit (nixpkgs) pkgs;
+
+  haskellPackages =
+    (if compiler == "default"
+       then pkgs.haskellPackages
+       else pkgs.haskell.packages.${compiler}).override {
+          overrides = self: super: {
+            tasty-hedgehog = self.callHackage "tasty-hedgehog" "0.2.0.0" {};
+            mkDerivation = args:
+              super.mkDerivation (args // { enableLibraryProfiling = profiling; });
+          };
+       };
+
+  drv = haskellPackages.callPackage ./wp-test.nix {};
+
+  devTools =
+    [ pkgs.cabal-install
+      pkgs.mariadb
+      haskellPackages.stylish-haskell
+      haskellPackages.apply-refact
+      haskellPackages.hlint
+      haskellPackages.hindent
+      haskellPackages.hasktags
+    ];
+
+  shellDrv = pkgs.haskell.lib.overrideCabal drv (drv': {
+    buildDepends =
+      (drv'.buildDepends or []) ++
+      devTools ++
+      [ (haskellPackages.hoogleLocal {
+          packages =
+            (drv'.libraryHaskellDepends or []) ++
+            (drv'.executableHaskellDepends or []) ++
+            (drv'.testHaskellDepends or []) ;
+        })
+      ];
+  });
+
 in
-  env.drv
+  if pkgs.lib.inNixShell then shellDrv.env else drv
