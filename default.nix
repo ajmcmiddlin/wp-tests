e27005b7568a87c919b7b7cbb39463d0c9ce8608
Andrew McCluskey <andrew@ajmccluskey.com>
2018-07-03 13:32:33 +1000

    Pin to latest dependent map
    Want dem lens functions

--- default.nix
+++ default.nix
@@ -3,12 +3,21 @@
 let
   inherit (nixpkgs) pkgs;
 
+  # Get those sweet lens functions
+  dmap = pkgs.fetchgit {
+    url = "https://github.com/mokus0/dependent-map";
+    rev = "40c481c48bbcd15c4183b89b925b560c07f0eb54";
+    sha256  = "1gyfscgqyazk2nm5pmgvc972k0m86xhjyq0lnz63rngq82y2242s";
+    fetchSubmodules = true;
+  };
+
   haskellPackages =
     (if compiler == "default"
        then pkgs.haskellPackages
        else pkgs.haskell.packages.${compiler}).override {
           overrides = self: super: {
             tasty-hedgehog = self.callHackage "tasty-hedgehog" "0.2.0.0" {};
+            dependent-map = self.callCabal2nix "dependent-map" dmap {};
             mkDerivation = args:
               super.mkDerivation (args // { enableLibraryProfiling = profiling; });
           };
